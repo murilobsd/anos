7547bb72f0ab4acce7acc6b74a128d9faefc880c
murilo ijanc <mbsd@m0x.ru>
2024-06-01 15:34:27 -0300

    feat: exit qemu when testing

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,30 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4); // see cargo.toml metadata (iobase)
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 /// This function is called on panic.
