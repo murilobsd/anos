5b9ac0594677499a6099d0a7bf2782ceb25ae83b
murilo ijanc <mbsd@m0x.ru>
2024-06-01 15:30:10 -0300

    feat: support unit test

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,21 @@
 
 #![no_std] // Disable links to the standard library
 #![no_main] // Don't want to use the normal entry point
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 /// This function is called on panic.
 #[panic_handler]
@@ -34,5 +45,15 @@ pub extern "C" fn _start() -> ! {
     // a named `_start` by default
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
