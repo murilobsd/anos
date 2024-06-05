5eacd339b929bbdefde08566a68757171f6d735d
murilo ijanc <mbsd@m0x.ru>
2024-06-05 17:55:33 -0300

    feat: Add double fault exception handler

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,10 @@ pub extern "C" fn _start() -> ! {
 
     anos::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    }
 
     #[cfg(test)]
     test_main();
