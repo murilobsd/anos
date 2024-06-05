68911d530e54b9bee1ac8bcc34f693d6fbb69a8e
murilo ijanc <mbsd@m0x.ru>
2024-06-05 17:13:07 -0300

    chore: Add test for checking cpu exception

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -32,3 +32,9 @@ pub fn init_dt() {
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
+
+#[test_case]
+fn test_breakpoint_exception() {
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+}
